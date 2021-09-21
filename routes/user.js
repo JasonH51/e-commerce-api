8678148a1f8e0313052c1ed1a2ae1eac20103506
JasonH51 <81194110+JasonH51@users.noreply.github.com>
2021-09-20 20:46:50 -0500

    token / verifyToken functions

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,58 @@
-const router = require('express').Router();
+const {verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin} = require('./verifyToken');
 
+const router = require('express').Router();
+const User = require('../models/User');
+
+router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString();
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body
+      },
+      {new: true}
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//DELETE
+router.delete('/:id', verifyTokenAndAuthorization, async (req, res, next) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json('User has been deleted...');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET USER
+router.get('/find/:id', verifyTokenAndAdmin, async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const {password, ...others} = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET ALL USER
+router.get('/', verifyTokenAndAdmin, async (req, res, next) => {
+    const query = req.query.new;
+  try {
+    const users = query ? await User.find().sort({_id: -1}).limit(5) : await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 module.exports = router;
